8d593eb9becbb4943598dfef5a1888979d6beb82
Patryk Kozlowski <pat.kozlowski00@gmail.com>
2019-06-30 10:55:15 +0200

    improved responsivness

--- src/components/stockPage.js
+++ src/components/stockPage.js
@@ -191,8 +191,8 @@ export default class stockPage extends React.Component {
       }
     }
   }
-  changeChartMinutes(minutes){
-    chartData1 = []
+  changeChartMinutes(minutes) {
+    chartData1 = [];
     const stockApi = `https://cloud.iexapis.com/stable/stock/${
       this.props.symbol
     }/intraday-prices?token=pk_95c4a35c80274553987b93e74bb825d7`;
@@ -202,6 +202,29 @@ export default class stockPage extends React.Component {
         for (let i = 0; i < minutes; i++) {
           if (result[i].average !== null) {
             chartData1.push(parseFloat(result[i].average).toFixed(2));
+            chartLength++;
+          }
+        }
+      })
+      .then(() => {
+        setTimeout(() => {
+          this.setState({
+            loaded: true
+          });
+        }, 1000);
+      });
+  }
+  getYTDChart() {
+    chartData1 = [];
+    const stockApi = `https://cloud.iexapis.com/stable/stock/${
+      this.props.symbol
+    }/chart/ytd?token=pk_95c4a35c80274553987b93e74bb825d7`;
+    fetch(stockApi)
+      .then((res) => res.json())
+      .then((result) => {
+        for (let i = 0; i < result.length; i++) {
+          if (result[i].average !== null) {
+            chartData1.push(parseFloat(result[i].close).toFixed(2));
             chartLength++;
           }
         }
@@ -244,7 +267,7 @@ export default class stockPage extends React.Component {
       .catch(function(error) {
         console.log("Error getting document:", error);
       });
-    this.getMinutesChart(30);
+    this.getYTDChart();
   }
   render() {
     const lastPrice = `https://cloud.iexapis.com/stable/stock/${
@@ -318,15 +341,13 @@ export default class stockPage extends React.Component {
             <svg
               className="leftbar__log"
               onClick={() => logout()}
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 30"
-              x="0px"
-              y="0px"
+              viewBox="0 0 512.016 512"
             >
-              <title>Log Out</title>
-              <g data-name="Log Out">
-                <path d="M13,21a1,1,0,0,1-1,1H3a1,1,0,0,1-1-1V3A1,1,0,0,1,3,2h9a1,1,0,0,1,0,2H4V20h8A1,1,0,0,1,13,21Zm8.92-9.38a1,1,0,0,0-.22-.32h0l-4-4a1,1,0,0,0-1.41,1.41L18.59,11H7a1,1,0,0,0,0,2H18.59l-2.29,2.29a1,1,0,1,0,1.41,1.41l4-4h0a1,1,0,0,0,.22-1.09Z" />
-              </g>
+              <path d="m496 240.007812h-202.667969c-8.832031 0-16-7.167968-16-16 0-8.832031 7.167969-16 16-16h202.667969c8.832031 0 16 7.167969 16 16 0 8.832032-7.167969 16-16 16zm0 0" />
+              <path d="m416 320.007812c-4.097656 0-8.191406-1.558593-11.308594-4.691406-6.25-6.253906-6.25-16.386718 0-22.636718l68.695313-68.691407-68.695313-68.695312c-6.25-6.25-6.25-16.382813 0-22.632813 6.253906-6.253906 16.386719-6.253906 22.636719 0l80 80c6.25 6.25 6.25 16.382813 0 22.632813l-80 80c-3.136719 3.15625-7.230469 4.714843-11.328125 4.714843zm0 0" />
+              <path d="m170.667969 512.007812c-4.566407 0-8.898438-.640624-13.226563-1.984374l-128.386718-42.773438c-17.46875-6.101562-29.054688-22.378906-29.054688-40.574219v-384c0-23.53125 19.136719-42.6679685 42.667969-42.6679685 4.5625 0 8.894531.6406255 13.226562 1.9843755l128.382813 42.773437c17.472656 6.101563 29.054687 22.378906 29.054687 40.574219v384c0 23.53125-19.132812 42.667968-42.664062 42.667968zm-128-480c-5.867188 0-10.667969 4.800782-10.667969 10.667969v384c0 4.542969 3.050781 8.765625 7.402344 10.28125l127.785156 42.582031c.917969.296876 2.113281.46875 3.480469.46875 5.867187 0 10.664062-4.800781 10.664062-10.667968v-384c0-4.542969-3.050781-8.765625-7.402343-10.28125l-127.785157-42.582032c-.917969-.296874-2.113281-.46875-3.476562-.46875zm0 0" />
+              <path d="m325.332031 170.675781c-8.832031 0-16-7.167969-16-16v-96c0-14.699219-11.964843-26.667969-26.664062-26.667969h-240c-8.832031 0-16-7.167968-16-16 0-8.832031 7.167969-15.9999995 16-15.9999995h240c32.363281 0 58.664062 26.3046875 58.664062 58.6679685v96c0 8.832031-7.167969 16-16 16zm0 0" />
+              <path d="m282.667969 448.007812h-85.335938c-8.832031 0-16-7.167968-16-16 0-8.832031 7.167969-16 16-16h85.335938c14.699219 0 26.664062-11.96875 26.664062-26.667968v-96c0-8.832032 7.167969-16 16-16s16 7.167968 16 16v96c0 32.363281-26.300781 58.667968-58.664062 58.667968zm0 0" />
             </svg>
           </div>
           {symbol}
@@ -363,9 +384,9 @@ export default class stockPage extends React.Component {
                           "flex";
                       document.getElementById(
                         "topbar__searchbar"
-                      ).style.boxShadow = "0px 0px 30px 0px rgba(0,0,0,0.17)";
+                      ).style.boxShadow = "0px 0px 30px 0px rgba(0,0,0,0.10)";
                       document.getElementById("results").style.boxShadow =
-                        "0px 0px 30px 0px rgba(0,0,0,0.17)";
+                        "0px 0px 30px 0px rgba(0,0,0,0.10)";
                     }}
                     onBlur={() => {
                       setTimeout(() => {
@@ -455,8 +476,22 @@ export default class stockPage extends React.Component {
                 <div className="stock__chart">
                   <Line data={this.data1} options={options} />
                   <div className="stockPage__timers">
-                    <h6 className="active">30 minutes</h6>
-                    <h6 onClick={()=>{this.changeChartMinutes(5)}}>5 minutes</h6>
+                    <h6
+                      className="active"
+                      onClick={() => {
+                        this.getYTDChart();
+                      }}
+                    >
+                      YTD
+                    </h6>
+                    <h6>30 minutes</h6>
+                    <h6
+                      onClick={() => {
+                        this.changeChartMinutes(5);
+                      }}
+                    >
+                      5 minutes
+                    </h6>
                   </div>
                 </div>
                 <div className="stockPage__trade">
