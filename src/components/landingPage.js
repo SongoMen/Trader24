d39b36cbd01c4eb24688400f6898d64b8a12cdba
Patryk Kozlowski <pat.kozlowski00@gmail.com>
2019-07-27 19:47:40 +0200

    landing page improvements,

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,13 +1,23 @@
 import React from "react";
-import LandingMenu from './LandingMenu'
+import LandingMenu from "./LandingMenu";
 
 const LandingPage = () => {
   return (
     <div className="landingPage">
-        <LandingMenu name="LOGIN" url="/login" name2="REGISTER" url2="/register"/>
+      <LandingMenu
+        name="LOGIN"
+        url="/login"
+        name2="REGISTER"
+        url2="/register"
+      />
       <div className="landing">
-        <h1>MEET THE NEW STANDARTS FOR <br/>MARKET TRADING</h1>
+        <h1>
+          MEET THE NEW STANDARTS FOR <br />
+          MARKET TRADING
+        </h1>
+        <img src={require("../images/mockup.png")} />
       </div>
+      <div className="background" />
     </div>
   );
 };
