29d20a9ebaaca11e6b3bf919dfd16d4f61df0b1e
SongoMen <songomengaming@gmail.com>
2019-07-30 13:17:04 +0200

    improvements

--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -12,12 +12,12 @@ const LandingPage = () => {
       />
       <div className="landing">
         <h1>MEET THE NEW STANDARTS FOR MARKET TRADING</h1>
-        <img src={require("../images/mockup.png")} alt="preview"/>
+        <img src={require("../images/mockup.png")} alt="preview" />
       </div>
       <div className="background">
         <svg width="100%" height="100">
-          <path d="M600 0 L0 0 L1200 1250 Z" />
-          <path d="M0 0 L0 2000 L1100 1040 Z" />
+          <path d="M600 0 L0 0 L3200 5250 Z" />
+          <path d="M0 0 L0 2000 L4100 4040 Z" />
         </svg>
       </div>
     </div>
