ca360942e3a137888a56787763398956646e6636
SongoMen <songomengaming@gmail.com>
2019-12-21 21:45:12 +0100

    fix

--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,23 +1,22 @@
-  /*
-   * returns difference between two numbers in %  
-   * @param {a} 1 number
-   * @param {b} 2 number
-   */
+/*
+ * returns difference between two numbers in %
+ * @param {a} 1 number
+ * @param {b} 2 number
+ */
 
 export function relDiff(a, b) {
-    return 100 * Math.abs((a - b) / ((a + b) / 2));
+  return 100 * Math.abs((a - b) / ((a + b) / 2));
 }
 
 /*
-* returns formatted number
-* @param {x} number
-*/
+ * returns formatted number
+ * @param {x} number
+ */
 
 export function numberWithCommas(x) {
-    if (typeof x !== 'undefined') {
-        return x.toLocaleString();
-    } else {
-        return '';
-    }
+  if (typeof x !== "undefined") {
+    return x.toLocaleString();
+  } else {
+    return "";
+  }
 }
-
