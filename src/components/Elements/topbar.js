751e22979594070e091bf005aa1f318c304de4e3
SongoMen <songomengaming@gmail.com>
2020-01-17 22:03:44 +0100

    converted all api keys into one fiel

--- src/components/Elements/topbar.js
+++ src/components/Elements/topbar.js
@@ -73,7 +73,7 @@ export default class Topbar extends React.Component {
     this._isMounted = true;
     let user = firebase.auth().currentUser.uid;
     fetch(
-      "https://cloud.iexapis.com/stable/ref-data/symbols?token=pk_95c4a35c80274553987b93e74bb825d7"
+      `https://cloud.iexapis.com/stable/ref-data/symbols?token=${process.env.REACT_APP_API_KEY_2}`
     )
       .then(res => res.json())
       .then(result => {
--- src/components/SymbolPage/stockPage.js
+++ src/components/SymbolPage/stockPage.js
@@ -302,7 +302,7 @@ export default class stockPage extends React.Component {
     labels = [];
     chartData1 = [];
     if (ytdChart.length === 0) {
-      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=pk_95c4a35c80274553987b93e74bb825d7&types=chart,quote&range=ytd`;
+      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=${process.env.REACT_APP_API_KEY_2}&types=chart,quote&range=ytd`;
       fetch(stockApi)
         .then(res => res.json())
         .then(result => {
@@ -340,7 +340,7 @@ export default class stockPage extends React.Component {
     labels = [];
     chartData1 = [];
     if (oneYear.length === 0) {
-      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=pk_95c4a35c80274553987b93e74bb825d7&types=chart,quote&range=1y`;
+      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=${process.env.REACT_APP_API_KEY_2}&types=chart,quote&range=1y`;
       fetch(stockApi)
         .then(res => res.json())
         .then(result => {
@@ -376,7 +376,7 @@ export default class stockPage extends React.Component {
     labels = [];
     chartData1 = [];
     if (twoYears.length === 0) {
-      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=pk_95c4a35c80274553987b93e74bb825d7&types=chart,quote&range=2y`;
+      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=${process.env.REACT_APP_API_KEY_2}&types=chart,quote&range=2y`;
       fetch(stockApi)
         .then(res => res.json())
         .then(result => {
@@ -412,7 +412,7 @@ export default class stockPage extends React.Component {
     labels = [];
     chartData1 = [];
     if (oneMonth.length === 0) {
-      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=pk_95c4a35c80274553987b93e74bb825d7&types=chart,quote&range=1m`;
+      const stockApi = `https://cloud.iexapis.com/beta/stock/${symbol}/batch?token=${process.env.REACT_APP_API_KEY_2}&types=chart,quote&range=1m`;
       fetch(stockApi)
         .then(res => res.json())
         .then(result => {
@@ -516,7 +516,7 @@ export default class stockPage extends React.Component {
 
   rendering() {
     fetch(
-      `https://cloud.iexapis.com/stable/stock/${symbol}/quote?displayPercent=true&token=pk_95c4a35c80274553987b93e74bb825d7`,
+      `https://cloud.iexapis.com/stable/stock/${symbol}/quote?displayPercent=true&token=${process.env.REACT_APP_API_KEY_2}`,
     )
       .then(res => res.json())
       .then(result => {
@@ -585,7 +585,7 @@ export default class stockPage extends React.Component {
       );
     document.title = `Trader24 - ${symbol}`;
     fetch(
-      `https://cloud.iexapis.com/stable/stock/${symbol}/quote?displayPercent=true&token=pk_95c4a35c80274553987b93e74bb825d7`,
+      `https://cloud.iexapis.com/stable/stock/${symbol}/quote?displayPercent=true&token=${process.env.REACT_APP_API_KEY_2}`,
     )
       .then(res => res.json())
       .then(result => {
@@ -599,7 +599,7 @@ export default class stockPage extends React.Component {
         if (this.state.marketStatus) {
           setInterval(() => {
             fetch(
-              `https://cloud.iexapis.com/stable/stock/${symbol}/quote?displayPercent=true&token=pk_95c4a35c80274553987b93e74bb825d7`,
+              `https://cloud.iexapis.com/stable/stock/${symbol}/quote?displayPercent=true&token=${process.env.REACT_APP_API_KEY_2}`,
             )
               .then(res => res.json())
               .then(result => {
@@ -721,7 +721,7 @@ export default class stockPage extends React.Component {
         }
       });
     fetch(
-      "https://cloud.iexapis.com/stable/ref-data/symbols?token=pk_95c4a35c80274553987b93e74bb825d7",
+      `https://cloud.iexapis.com/stable/ref-data/symbols?token=${process.env.REACT_APP_API_KEY_2}`,
     )
       .then(res => res.json())
       .then(result => {
