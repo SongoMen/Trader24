ca6a53aa809c8b906a9437ba801e066079f1c5e4
SongoMen <34171300+SongoMen@users.noreply.github.com>
2020-04-19 23:52:54 +0200

    style

--- src/components/Dashboard/Chart.js
+++ src/components/Dashboard/Chart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Line} from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import PropTypes from "prop-types";
 
 import Loader from "../Elements/Loader";
